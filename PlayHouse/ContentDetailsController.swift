4aa584112a9111ce8febd32ae7db1af4036a3294
Shubham-Langade <shubhamlangade@gmail.com>
2023-01-03 16:42:29 +0530

    ContentDetails screen added

---
+++ PlayHouse/ContentDetailsController.swift
@@ -0,0 +1,30 @@
+//
+//  ContentDetailsController.swift
+//  PlayHouse
+//
+//  Created by shubhan.langade on 03/01/23.
+//
+
+import UIKit
+
+class ContentDetailsController: UIViewController {
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PlayHouse/FirstCollection.swift
@@ -0,0 +1,27 @@
+//
+//  FirstCollection.swift
+//  PlayHouse
+//
+//  Created by shubhan.langade on 02/01/23.
+//
+
+import UIKit
+
+class FirstCollection: UICollectionViewCell {
+    let myImageView = UIImageView()
+   
+   func config(cellWith image : UIImage){
+       myImageView.translatesAutoresizingMaskIntoConstraints = false
+       self.addSubview(myImageView)
+       myImageView.image = image
+       myImageView.contentMode = .scaleToFill
+       myImageView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor).isActive = true
+       //myImageView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
+       print("height \(self.frame.height)")
+       print("width \(self.frame.width)")
+       myImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 0).isActive = true
+       myImageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: 0).isActive = true
+       myImageView.heightAnchor.constraint(equalToConstant: self.frame.height).isActive = true
+       myImageView.widthAnchor.constraint(equalToConstant: self.frame.width).isActive = true
+   }
+}
