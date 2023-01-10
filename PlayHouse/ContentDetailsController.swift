dd63c5ad7759d9335ec34641d9899af4259165b8
Shubham-Langade <shubhamlangade@gmail.com>
2023-01-10 12:38:00 +0530

    ContentDetails screen implemented

--- PlayHouse/ContentDetailsController.swift
+++ PlayHouse/ContentDetailsController.swift
@@ -9,22 +9,55 @@ import UIKit
 
 class ContentDetailsController: UIViewController {
     @IBOutlet weak var myCollectionView: UICollectionView!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var moreLikeThisCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let gradient = CAGradientLayer()
+        gradient.frame = thumbnailImageView.bounds
+        gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradient.locations = [0.0, 0.99]
+        thumbnailImageView.layer.insertSublayer(gradient, at: 0)
+        myCollectionView.delegate = self
+        myCollectionView.dataSource = self
+        moreLikeThisCollectionView.delegate = self
+        moreLikeThisCollectionView.dataSource = self
+        // Do any additional setup after loading the view. SecondCollection
+    }
+}
+extension ContentDetailsController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == myCollectionView{
+        return 10
+        }
+        else if collectionView == moreLikeThisCollectionView{
+        return 30
+        }
+        return 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == myCollectionView{
+            let cell = myCollectionView.dequeueReusableCell(withReuseIdentifier: "FirstCollection", for: indexPath) as! FirstCollection
+            cell.config(cellWith: UIImage(named: "Moneyheist")!)
+                    return cell
+        }
+        else if collectionView == moreLikeThisCollectionView{
+            let cell = moreLikeThisCollectionView.dequeueReusableCell(withReuseIdentifier: "SecondCollection", for: indexPath) as! SecondCollection
+            cell.config(cellWith: UIImage(named: "Moneyheist")!)
+                    return cell
+        }
+        return UICollectionViewCell()
     }
-    */
-
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+        {
+            if collectionView == myCollectionView{
+                return CGSize(width: 100.0, height: 100.0)
+            }
+            else if collectionView == moreLikeThisCollectionView{
+                return CGSize(width: 110.0, height: 200.0)
+            }
+            return CGSize(width: 0, height: 0)
+        }
 }
